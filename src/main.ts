90f012e2772e601aa35e033be2ade80fd9764e6e
Santiago Velásquez <santyvano@outlook.com>
2023-02-04 07:53:49 -0500

    agregando archivos de produccion

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@
   import { apiAxios,} from './counter'
   import { Product } from './models'
 import { drawData, getpaginatedProducts } from './utils'
+(async()=>{
+  const {data}= await apiAxios<Product[]>(`?offset=0&limit=10`);
+  drawData(data,"")
+  window.addEventListener("scroll",getpaginatedProducts)
+})()
 
-const {data}= await apiAxios<Product[]>(`?offset=0&limit=10`);
-drawData(data,"")
-
-window.addEventListener("scroll",getpaginatedProducts)
 
